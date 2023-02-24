6fd268f6137d8f412d85efc9b353d01b1cf58d87
Marina-Chuhlib <marinachuhlib@gmail.com>
2023-02-24 12:16:47 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-// import { store } from '../src/components/redux/store';
 import { store } from '../src/components/redux/store';
 import App from 'components/App';
 import './index.css';
