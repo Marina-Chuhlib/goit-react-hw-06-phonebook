40a7ad9160e14176ad49fa550bade7358a745c3a
Marina-Chuhlib <marinachuhlib@gmail.com>
2023-02-24 12:13:32 +0300

    Update homepage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,75 @@
-export const App = () => {
+import { useSelector, useDispatch } from 'react-redux';
+
+import {
+  getAllContacts,
+  getFilteredContacts,
+  getFilter,
+} from './redux/selectors';
+import { addContact, deleteContact, setFilter } from './redux/actions';
+
+import ContactForm from './ContactForm/ContactForm';
+import ContactsList from './ContactsList/ContactsList';
+import Filter from './Filter/Filter';
+
+import css from './App.module.css';
+
+const App = () => {
+  const contacts = useSelector(getAllContacts);
+  console.log(contacts);
+
+  const filter = useSelector(getFilter);
+
+  const filteredContacts = useSelector(getFilteredContacts);
+
+  const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   localStorage.setItem('my-contacts', JSON.stringify(contacts));
+  // }, [contacts]);
+
+  const isDuplicate = name => {
+    const normalized = name.toLowerCase();
+    const result = contacts.find(({ name }) => {
+      return normalized === name.toLowerCase();
+    });
+    return Boolean(result);
+  };
+
+  const handleAddContact = ({ name, number }) => {
+    if (isDuplicate(name)) {
+      alert(`${name} is already in contacts`);
+      return false;
+    }
+
+    const action = addContact({ name, number });
+    dispatch(action);
+  };
+
+  const handleDeleteContact = id => {
+    dispatch(deleteContact(id));
+  };
+
+  const handleFilter = ({ target }) => {
+    dispatch(setFilter(target.value));
+  };
+
+  const isContacts = Boolean(contacts.length > 0);
+
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.wrapper}>
+      <h2>Phonebook</h2>
+      <ContactForm onSubmit={handleAddContact} />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={handleFilter} />
+
+      {isContacts && (
+        <ContactsList contacts={filteredContacts} deleteContact={handleDeleteContact} />
+      )}
+      {!isContacts && <p className={css.text}>No contacts in list</p>}
     </div>
   );
 };
+
+export default App;
---
+++ src/components/redux/reducer.js
@@ -0,0 +1,23 @@
+import { ADD_CONTACT, DELETE_CONTACT, SET_FILTER } from './types';
+// import contacts from 'components/contacts';
+
+const initialState = {
+  contacts: [],
+  filter: '',
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_CONTACT:
+      const newContacts = [...state.contacts, action.payload];
+      return { ...state, contacts: newContacts };
+
+    case DELETE_CONTACT:
+      const result = state.contacts.filter(item => item.id !== action.payload);
+      return { ...state, contacts: result };
+    case SET_FILTER:
+      return { ...state, filter: action.payload };
+    default:
+      return state;
+  }
+};
